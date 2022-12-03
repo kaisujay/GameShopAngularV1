76d7038695cb35502a12a4c9766527ba7d8b08aa
kaisujay <kai.sujay@gmail.com>
2022-12-03 15:34:39 +0530

    Added Routing for "ContactUs" items

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutusComponent } from './components/header/contactus/aboutus/aboutus.component';
+import { FaqComponent } from './components/header/contactus/faq/faq.component';
+import { NewsComponent } from './components/header/contactus/news/news.component';
+import { SupportComponent } from './components/header/contactus/support/support.component';
+import { GameComponent } from './components/header/game/game.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'Support', component: SupportComponent },
+  { path: 'News', component: NewsComponent },
+  { path: 'FAQ', component: FaqComponent },
+  { path: 'AboutUs', component: AboutusComponent }  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
