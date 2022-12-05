0d00dd324cf695c1d75bbfba31eab71167890a2c
kaisujay <kai.sujay@gmail.com>
2022-12-05 11:28:11 +0530

    Added "Player Service" for Display records

---
+++ src/app/services/player/player-service.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { displayPlayer } from 'src/app/models/player/display-player.models';
+import { logInPlayer } from 'src/app/models/player/login-player.models';
+import { registerPlayer } from 'src/app/models/player/register-player.models';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PlayerServiceService {
+
+  registerPlayer:registerPlayer;
+  logInPlayer:logInPlayer;
+  displayPlayer:displayPlayer;
+
+  constructor(private http:HttpClient) { }
+
+  apiUrl:string="http://localhost:12969/api/player";
+
+  getPlayerById(value:string){
+    this.http.get<displayPlayer>(this.apiUrl+"/byid/"+value);
+  }
+
+  getPlayerByUserName(value:string){
+    this.http.get<displayPlayer>(this.apiUrl+"/byusername/"+value);
+  }
+}
